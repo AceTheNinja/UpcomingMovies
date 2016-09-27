e776f4bc9f13f6731f49d0f137310b02f91ad19f
AceTheNinja <raitapan1@gmail.com>
2016-09-27 22:18:20 +0530

    Added pagination and loader

--- js/app.js
+++ js/app.js
@@ -2,10 +2,21 @@
 
 app.controller('mainController', ['$scope', '$http', function ($scope, $http) {
 
-    $http.get("http://api.themoviedb.org/3/movie/upcoming?api_key=a7f4e7531c0abfef4ade0b794873f5ce")
+    $scope.page = 1;
+
+    $http.get("http://api.themoviedb.org/3/movie/upcoming?api_key=a7f4e7531c0abfef4ade0b794873f5ce&page=" + $scope.page)
         .then(function (response) {
             $scope.data = response.data;
         });
+
+    $scope.movieget = function (page) {
+        $scope.page = page;
+        console.log($scope.page);
+        $http.get("http://api.themoviedb.org/3/movie/upcoming?api_key=a7f4e7531c0abfef4ade0b794873f5ce&page=" + $scope.page)
+            .then(function (response) {
+                $scope.data = response.data;
+            });
+    };
 
     $scope.movData = function (id) {
         $http.get("http://api.themoviedb.org/3/movie/" + id + "?api_key=a7f4e7531c0abfef4ade0b794873f5ce")
@@ -15,7 +26,8 @@ app.controller('mainController', ['$scope', '$http', function ($scope, $http) {
             });
     };
 
-    $scope.clear = function () {
-        $scope.movie = [];
+    $scope.getTimes = function (n) {
+        return new Array(n);
     };
+
 }]);
